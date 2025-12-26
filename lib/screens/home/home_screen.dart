95bc11cf57d514c6a678b489987cd9def985e592
Iktiar Nazib <iktiarnazib@gmail.com>
2025-12-26 18:36:51 +0600

    Manual notifier implementation

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_files/models/product.dart';
+import 'package:riverpod_files/providers/cart_provider.dart';
 import 'package:riverpod_files/providers/products_provider.dart';
 import 'package:riverpod_files/shared/cart_icon.dart';
 
@@ -10,6 +11,7 @@ class HomeScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final List<Product> allProducts = ref.watch(productsProvider);
+    final cartProduct = ref.watch(cartNotifierProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -34,11 +36,29 @@ class HomeScreen extends ConsumerWidget {
                 children: [
                   Image.asset(
                     allProducts[index].image,
-                    height: 100,
-                    width: 100,
+                    height: 80,
+                    width: 80,
                   ),
                   Text(allProducts[index].title),
-                  Text('\$${allProducts[index].price}')
+                  Text('\$${allProducts[index].price}'),
+                  if (cartProduct.contains(allProducts[index]))
+                    OutlinedButton(
+                        onPressed: () {
+                          ref
+                              .read(cartNotifierProvider.notifier)
+                              .removeProduct(allProducts[index]);
+                        },
+                        child: const Text('Remove')),
+                  if (!cartProduct.contains(allProducts[index]))
+                    OutlinedButton(
+                        onPressed: () {
+                          //to access method, we use ref.read(notifiername.notifier).methodName to make changes
+                          ref
+                              .read(cartNotifierProvider.notifier)
+                              .addProduct(allProducts[index]);
+                          //in riverpod when reading read variable, we write ref.watch(provider_Name)
+                        },
+                        child: const Text('Add to Cart'))
                 ],
               ),
             );
